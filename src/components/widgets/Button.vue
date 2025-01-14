5cbe0fe66abfc512fd51f2b20327658b0d7ee8c4
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-01-14 14:24:03 -0500

    Add Button widget

---
+++ src/components/widgets/Button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  const props = defineProps({
+    text:  { type: String, required: true },
+    class: { type: String, required: false, default: 'primary' },
+  })
+</script>
+
+<template>
+  <button :class=props.class>{{ props.text }}</button>
+</template>
+
+<style scoped>
+  button {
+    all: unset;
+    box-sizing: border-box;
+    cursor: pointer;
+    opacity: 0.85;
+    padding: 15px;
+    transition: 0.50s;
+    width: 100%;
+  }
+
+  button:hover {
+    opacity:1;
+  }
+
+  button.primary {
+    background: #04AA6D;
+    color: #ffffff;
+  }
+</style>
