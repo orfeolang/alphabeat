8905b68da5677a584d25fab89f35bf49171f422c
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-01-27 08:15:23 -0500

    Add dependency: vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './assets/css/reset.css'
 import './assets/css/font.css'
 import './assets/css/style.css'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
