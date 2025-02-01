c0dfbeab6248b9375978aa742f5d51a330348724
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-02-01 16:16:19 -0500

    Improve string helper function names

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,11 +1,11 @@
-const isEmpty = (s:string):boolean => s.trim() === ''
-const isOverLength = (s:string, l:number):boolean => s.trim().length > l
+const isEmptyString = (s:string):boolean => s.trim() === ''
+const isOverLengthString = (s:string, l:number):boolean => s.trim().length > l
 const isPossiblyEmail = (s:string):boolean => /^[^@]+@[^@]+\.[^@]+$/.test(s)
 const isWordWithHyphens = (s:string):boolean => /^[_a-zA-Z\d-]+$/.test(s)
 
 export {
-  isEmpty,
-  isOverLength,
+  isEmptyString,
+  isOverLengthString,
   isPossiblyEmail,
   isWordWithHyphens,
 }
--- src/helpers/validator.ts
+++ src/helpers/validator.ts
@@ -1,13 +1,13 @@
 import {
-  isEmpty,
-  isOverLength,
+  isEmptyString,
+  isOverLengthString,
   isPossiblyEmail,
   isWordWithHyphens,
 } from './string'
 
 const validateEmail = (email:string):Array<string> => {
   const errors = []
-  if (isEmpty(email)) {
+  if (isEmptyString(email)) {
     errors.push('Your email is required.')
   }
   else if (email.length > 0 && !isPossiblyEmail(email)) {
@@ -18,7 +18,7 @@ const validateEmail = (email:string):Array<string> => {
 
 const validatePassword = (password:string):Array<string> => {
   const errors = []
-  if (isEmpty(password)) {
+  if (isEmptyString(password)) {
     errors.push('Your password is required.')
   }
   return errors
@@ -26,11 +26,11 @@ const validatePassword = (password:string):Array<string> => {
 
 const validateUsername = (username:string, maxLength: number):Array<string> => {
   const errors = []
-  if (isEmpty(username)) {
+  if (isEmptyString(username)) {
     errors.push('Your username is required.')
   }
   else {
-    if (isOverLength(username, maxLength)) {
+    if (isOverLengthString(username, maxLength)) {
       errors.push(`Your username cannot be more than ${maxLength} characters long.`)
     }
     if ( ! isWordWithHyphens(username)) {
