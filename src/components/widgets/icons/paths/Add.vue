5e186e4cbde34fe8ffb1bda8ce7670d4598eff64
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-02-09 22:47:52 -0500

    Fix widgets/icons/paths/Add.vue

--- src/components/widgets/icons/paths/Add.vue
+++ src/components/widgets/icons/paths/Add.vue
@@ -1,11 +1,3 @@
 <template>
   <path d="M17 15L17 8 15 8 15 15 8 15 8 17 15 17 15 24 17 24 17 17 24 17 24 15z"></path>
-  <rect
-     style="opacity:0.18;fill:#ffff00;stroke-width:1.83685"
-     id="rect4"
-     width="32"
-     height="32"
-     x="0"
-     y="0"
-  />
 </template>
