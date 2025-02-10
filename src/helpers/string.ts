488aa21da230de587830d5b7121377fdb10fe9a5
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-02-10 00:54:46 -0500

    Update helpers: string

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,11 +1,11 @@
 const isEmptyString = (s:string):boolean => s.trim() === ''
 const isOverLengthString = (s:string, l:number):boolean => s.trim().length > l
-const isPossiblyEmail = (s:string):boolean => /^[^@]+@[^@]+\.[^@]+$/.test(s)
+const isValidEmail = (s:string):boolean => /^[^@]+@[^@]+\.[^@]+$/.test(s)
 const isWordWithHyphens = (s:string):boolean => /^[_a-zA-Z\d-]+$/.test(s)
 
 export {
   isEmptyString,
   isOverLengthString,
-  isPossiblyEmail,
+  isValidEmail,
   isWordWithHyphens,
 }
