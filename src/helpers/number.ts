5026ae033f72bcb148067e4201ee97f2a40e1f4a
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-02-10 00:53:46 -0500

    Update helpers: number

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -1,6 +1,31 @@
 const constrainNumber = (n:number, min:number, max:number):number =>
   Math.min(Math.max(n, min), max)
 
+const isNumberHigherThanBound = (n:number, bound:number):boolean => n > bound
+const isNumberLowerThanBound = (n:number, bound:number):boolean => n < bound
+
+const isNumberWithinBounds =
+  (n:number, min:number|undefined, max:number|undefined):boolean =>
+    (typeof min === 'undefined' || ! isNumberLowerThanBound(n, min)) &&
+    (typeof max === 'undefined' || ! isNumberHigherThanBound(n, max))
+
+const isNumeric = (s:string):boolean => {
+  // ! isNaN(+s):
+  //   Use type coercion to parse the entirely of the string.
+  //   parsefloat alone does not do this.
+  // ! isNaN(parseFloat(s)):
+  //   Ensure strings of whitespace fail.
+  return ! isNaN(+s) && ! isNaN(parseFloat(s))
+}
+
+const round = (n:number, numDecimals:number):number =>
+  +(n.toFixed(numDecimals))
+
 export {
-  constrainNumber
+  constrainNumber,
+  isNumberHigherThanBound,
+  isNumberLowerThanBound,
+  isNumberWithinBounds,
+  isNumeric,
+  round,
 }
